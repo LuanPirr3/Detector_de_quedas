53608c9fe656e967aab588a5ed5c7ccad7b5ad91
LuanPirr3 <lu4n.engs0ft@gmail.com>
2025-02-15 14:06:52 -0300

    update new coodirnate

--- Controle_de_Quedas/detector_quedas_yt-main/main.py
+++ Controle_de_Quedas/detector_quedas_yt-main/main.py
@@ -1,26 +1,39 @@
-
 import cv2
 import cvzone
 from cvzone.PoseModule import PoseDetector
+import math
 
-video = cv2.VideoCapture('vd01.mp4')
+video = cv2.VideoCapture('vd05.mp4')
 detector = PoseDetector()
 
 while True:
-    check,img = video.read()
-    img = cv2.resize(img,(1280,720))
+    check, img = video.read()
+    img = cv2.resize(img, (1280, 720))
+
     resultado = detector.findPose(img)
-    pontos,bbox = detector.findPosition(img,draw=False)
-    if len(pontos)>=1:
-        x,y,w,h = bbox['bbox']
-        cabeca = pontos[0][1]
-        joelho = pontos[26][1]
-        # print(cabeca,joelho)
-        diferenca = joelho-cabeca
+    pontos, bbox = detector.findPosition(img, draw=False)
 
-        if diferenca <=0:
-            cvzone.putTextRect(img,'QUEDA DETECTADA',(x,y-80),scale=3,thickness=3,colorR=(0,0,255))
+    if len(pontos) >= 1:
+        x, y, w, h = bbox['bbox']
+        cabeca = pontos[0][1], pontos[0][2]  # Cabeça: ponto
+        ombro = pontos[11][1], pontos[11][2]  # Ombro esquerdo
+        quadril = pontos[23][1], pontos[23][2]  # Quadril esquerdo
+        joelho = pontos[26][1], pontos[26][2]  # Joelho esquerdo
 
+        # Calcular ângulo entre ombro e quadril (ângulo do tronco)
+        delta_x = quadril[0] - ombro[0]
+        delta_y = quadril[1] - ombro[1]
+        angulo_tronco = math.degrees(math.atan2(delta_y, delta_x))
 
-    cv2.imshow('IMG',img)
+        # Verificar se o ângulo do tronco é quase horizontal
+        if -30 < angulo_tronco < 30:
+            print("Possível Queda Detectada!")
+            cvzone.putTextRect(img, 'QUEDA DETECTADA', (x, y - 80), scale=3, thickness=3, colorR=(0, 0, 255))
+
+        # Verificar se a posição da cabeça está muito próxima do chão (indicando uma queda)
+        if cabeca[1] > joelho[1]:  # cabeça estiver mais baixa que o joelho
+            print("Possível Queda Detectada!")
+            cvzone.putTextRect(img, 'QUEDA DETECTADA', (x, y - 80), scale=3, thickness=3, colorR=(0, 0, 255))
+
+    cv2.imshow('IMG', img)
     cv2.waitKey(1)
